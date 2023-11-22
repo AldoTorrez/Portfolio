9c64690fa3fec7ceaabec2b0aaf520a447a0b6b0
AldoTorrez <adlotorrez91@gmail.com>
2023-11-21 23:43:42 -0400

    Portfolio update version 2.0

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const develop = document.getElementById('develop');
-const texto = 'Full Stack Web Developer';
-let index = 0;
+const imageSkills = ["../assets/html-5.png", "../assets/css-3.png", "../assets/js.png", "../assets/jquery.png", "../assets/react.png", "../assets/redux.png", "../assets/node.png", "../assets/express.png", "../assets/postgress.png", "../assets/sequelize.png", "../assets/git.png", "../assets/github.png", "../assets/c++.png"]
+const textSkills = ["HTML5", "CSS3", "JavaScript", "jQuery", "React", "Redux", "NodeJS", "Express", "PostgreSQL", "Sequelize", "Git", "GitHub", "C++"]
 
-setInterval(()=>{
-    index++;
-    develop.textContent = texto.slice(0, index);
-    if(index === texto.length){
-        index = 0;
-    }
-}, 100)
+const cardSkills = document.querySelector('.container-card-tecnologias-find')
+
+imageSkills.map((img, index)=>{
+    let imgCard = document.createElement('img')
+    let divCard = document.createElement('div')
+    let pCard = document.createElement('p')
+
+    imgCard.setAttribute('src', img)
+    imgCard.setAttribute('alt', textSkills[index])
+    
+    imgCard.classList.add('tecnologias-image')
+    divCard.classList.add('container-card-tecnologias')
+    pCard.classList.add('tecnologias-text')
+    pCard.textContent = textSkills[index]
+    
+    divCard.appendChild(imgCard)
+    divCard.appendChild(pCard)
+    cardSkills.appendChild(divCard)
+})
