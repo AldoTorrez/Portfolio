3923ea5be200dbdbe24f7a699b9f90f897938338
Barcoder <adlotorrez91@gmail.com>
2024-08-17 15:41:27 -0400

    update responsive

--- script.js
+++ script.js
@@ -19,11 +19,21 @@ $(document).ready(function () {
 
   const desplegarMenu = ()=>{
     let menu = document.querySelector(".container-section")
-    if(menu.style.display === "flex"){
-      menu.style.display = "none"
+    let menu2 = document.querySelector("#menu2")
+    let menu3 = document.querySelector("#menu3")
+    let lis = document.querySelector(".container-section li")
+    if(menu.style.width !== "0px"){
+      menu.style.width = "0px"
+      menu.style.height = "0px"
+      menu2.style.width = "25px"
+      menu3.style.width = "25px"
     }
     else{
+      lis.style.opacity = "1"
       menu.style.backgroundColor = "black"
-      menu.style.display = "flex"
+      menu.style.width = "360px"
+      menu.style.height = "300px"
+      menu2.style.width = "20px"
+      menu3.style.width = "10px"
     }
   }
